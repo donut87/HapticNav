fc4fd33d5583fc83128a52d75b349f2c2d02f5f3
Christian Dohnert <donut87@gmail.com>
2015-06-13 13:18:38 +0200

    added a button

--- wear/src/main/java/donut87/de/navigationvibe/NavigatorActivity.java
+++ wear/src/main/java/donut87/de/navigationvibe/NavigatorActivity.java
@@ -1,8 +1,12 @@
 package donut87.de.navigationvibe;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class NavigatorActivity extends Activity {
@@ -20,5 +24,20 @@ public class NavigatorActivity extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
+
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        final Vibration vibration = new Vibration(v);
+
+        final Button round_button = (Button) findViewById(R.id.button);
+        final Button rect_button = (Button) findViewById(R.id.button_click);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vibration.vibrate(0.1, Vibration.side.LEFT);
+            }
+        };
+        round_button.setOnClickListener(listener);
+
     }
 }
